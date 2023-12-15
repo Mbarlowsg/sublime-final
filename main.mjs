52d82d44417aeae3387363d43060be010dcd47fe
Mbarlowsg <130187725+Mbarlowsg@users.noreply.github.com>
2023-12-15 00:53:23 -0500

    added total counter and scaled image

--- main.mjs
+++ main.mjs
@@ -4,9 +4,11 @@ import {imageList} from "./modules/imageList.mjs";
 const result = document.getElementById("result");
 const imageDisplay = document.getElementById('image_display'); // div to contain image
 const submitButtons = document.getElementsByClassName('submit_button');
-
+const displayedTotal = document.getElementById("total");
 // Script variables
 let chosenImage = {};
+let totalImages = 0;
+let totalCorrect = 0;
 
 window.onload = () => {
     // Setup buttons
@@ -25,9 +27,26 @@ window.onload = () => {
 function checkImage(image, button) {
     if (image.source === "real"){
         button.value === "real" ? result.innerText = "Result: Correct": result.innerText = "Result: Wrong";
+        if (button.value === "real"){
+            result.innerText = "Result: Correct";
+            totalImages++;
+            totalCorrect++;
+        } else {
+            totalImages++;
+            result.innerText = "Result: Wrong";
+        }
     } else if (image.source === "AI"){ 
         button.value === "fake" ? result.innerText = "Result: Correct": result.innerText = "Result: Wrong";   
+        if (button.value === "fake"){
+            result.innerText = "Result: Correct";
+            totalImages++;
+            totalCorrect++;
+        } else {
+            totalImages++;
+            result.innerText = "Result: Wrong";
+        }
     }
+    total.innerText = `Total: ${totalCorrect}/${totalImages}`
 }
 
 function chooseImage() {
